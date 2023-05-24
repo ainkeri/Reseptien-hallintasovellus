08fb0ef4cf592c4a114999ed0c7d4208ab2d6100
Inkeri Ahlström <inkeri.ahlstrom@helsinki.fi>
2023-05-24 12:32:56 +0300

    schema and other py files

---
+++ src/schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE,
+    password TEXT
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    picture DATA,
+    user_id INTEGER REFERENCES users,
+    posted_at TIMESTAMP
+);
+
+CREATE TABLE comments (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    user_id INTEGER REFERENCES users,
+    post_id INTEGER REFERENCES posts,
+    sent_at TIMESTAMP
+);
+
+CREATE TABLE likes (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    post_id INTEGER REFERENCES posts,
+);
+
+CREATE TABLE favorites (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    post_id INTEGER REFERENCES posts,
+);
