33aa7d52f79be91ca295d34d95e0ce5b3f1ffac9
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-05-31 14:21:26 +0300

    db modification

--- src/db.py
+++ src/db.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://inkeriahlstrom"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///ainkeri"
     db.init_app(app)
 
