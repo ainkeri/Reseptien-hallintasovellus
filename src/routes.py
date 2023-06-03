ffc913b21dc0b3fe83b427c67d51aecf90669bc3
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-06-03 19:21:12 +0300

    Clicking on recipes and seeing their information

--- src/routes.py
+++ src/routes.py
@@ -11,14 +11,15 @@ def homepage():
 
 @routes.route("/main")
 def main():
-    sql = text("SELECT P.content, U.username, P.posted_at FROM posts P, users U WHERE P.user_id=U.id ORDER BY P.id")
+    sql = text("SELECT P.id, P.content, U.username, P.posted_at FROM posts P, users U WHERE P.user_id=U.id ORDER BY P.id")
     recipes = db.session.execute(sql)
     post_list = recipes.fetchall()
     return render_template("main.html", count=len(post_list), recipes=post_list)
 
-@routes.route("/recipe")
-def recipe():
-    sql = text("SELECT P.content, P.ingredients, P.instructions FROM posts P, users U WHERE P.user_id=U.id ORDER BY P.id")
-    full_recepy = db.session.execute(sql)
-    list = full_recepy.fetchone()
-    return render_template("recipe.html", full_recipy=list)
+@routes.route("/recipe/<int:recipe_id>")
+def recipe(recipe_id):
+    sql = text("SELECT content, ingredients, instructions FROM posts WHERE id=:recipe_id")
+    recipe = db.session.execute(sql, {"recipe_id":recipe_id}).fetchone()
+    if recipe is None:
+        return render_template("error.html", message="Recipe not found")
+    return render_template("recipe.html", recipe=recipe)
