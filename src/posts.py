61f0b62445c5febf49d58147be7a201490ae2fdb
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-06-04 14:36:40 +0300

    modifying posts

--- src/posts.py
+++ src/posts.py
@@ -14,8 +14,10 @@ def send():
     user_id = session.get("user_id", 0)
     ingredients = request.form.get("ingredients")
     instructions = request.form.get("instructions")
+
     if user_id == 0:
         return render_template("error.html", message="Post failed")
+    
     sql = text("INSERT INTO posts (content, user_id, posted_at, ingredients, instructions) VALUES (:content, :user_id, NOW(), :ingredients, :instructions)")
     db.session.execute(sql, {"content":content, "user_id":user_id, "ingredients":ingredients, "instructions":instructions})
     db.session.commit()
@@ -24,3 +26,26 @@ def send():
 @posts.route("/recipe")
 def recipe():
     return render_template("recipe.html")
+
+@posts.route("/edit/<string:recipe_name>", methods=["GET", "POST"])
+def edit(recipe_name):
+    if request.method == "GET":
+        sql = text("SELECT content, ingredients, instructions FROM posts WHERE id = :recipe_id")
+        recipe = db.session.execute(sql, {"recipe_name": recipe_name}).fetchone()
+
+        if recipe is None:
+            return render_template("error.html", message="Recipe not found.")
+    
+        return render_template("edit_post.html", recipe=recipe)
+
+    elif request.method == "POST":
+        content = request.form.get("content")
+        ingredients = request.form.get("ingredients")
+        instructions = request.form.get("ingredients")
+
+        sql = text("UPDATE posts SET content = :content, ingredients = :ingredients, instructions = :instructions WHERE id = :recipe_name")
+        db.session.execute(sql, {"content": content, "ingredients": ingredients, "instructions": instructions, "recipe_name": recipe_name})
+        db.session.commit()
+
+        return render_template("recipe.html")
+
