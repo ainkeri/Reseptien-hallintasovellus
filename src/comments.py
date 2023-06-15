d4f9258e3d8468e2c4423bf9a1b2f36c1d1a661d
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-06-15 17:25:42 +0300

    Made functions to save comments into database.

--- src/comments.py
+++ src/comments.py
@@ -1,9 +1,22 @@
 from .db import db
-from flask import Blueprint, render_template, request, redirect, url_for, session, abort
+from flask import Blueprint, render_template, request, redirect, url_for, session
 from sqlalchemy import text
 
 comments = Blueprint("comments", __name__)
 
-@comments.route("/comments_list")
-def comments_list():
-    return render_template("comments_list.html")
+@comments.route("/new_comment")
+def new_comment():
+    return render_template("new_comment.html")
+
+@comments.route("/send_comment", methods=["POST"])
+def send_comment():
+    content = request.form.get("content")
+    post_id = request.form.get("post_id")
+    user_id = session.get("user_id", 0)
+
+
+    sql = text("INSERT INTO comments (content, user_id, post_id, sent_at) VALUES (:content, :user_id, :post_id, NOW())")
+    db.session.execute(sql, {"content":content, "user_id":user_id, "post_id":post_id})
+    db.session.commit()
+    return redirect(url_for("routes.comments_list"))
+
