56372b51769f5e16a0c8ebdb4b6e58203a3055fc
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-06-16 12:55:10 +0300

    Trying to make code work.

--- src/comments.py
+++ src/comments.py
@@ -4,19 +4,23 @@ from sqlalchemy import text
 
 comments = Blueprint("comments", __name__)
 
-@comments.route("/new_comment")
-def new_comment():
-    return render_template("new_comment.html")
+@comments.route("/new_comment/<int:post_id>")
+def new_comment(post_id):
+    post_id = request.form.get("post_id")
+    return render_template("new_comment.html", post_id=post_id)
 
-@comments.route("/send_comment", methods=["POST"])
-def send_comment():
+@comments.route("/send_comment/<int:post_id>", methods=["POST"])
+def send_comment(post_id):
     content = request.form.get("content")
     post_id = request.form.get("post_id")
     user_id = session.get("user_id", 0)
 
+    if not post_id:
+        return render_template("error.html", message="Invalid post ID.")
 
     sql = text("INSERT INTO comments (content, user_id, post_id, sent_at) VALUES (:content, :user_id, :post_id, NOW())")
     db.session.execute(sql, {"content":content, "user_id":user_id, "post_id":post_id})
     db.session.commit()
-    return redirect(url_for("routes.comments_list", recipe_id=post_id))
+    return redirect(url_for("routes.comments_list", post_id=post_id))
 
+ 
