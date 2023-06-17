03b507fa671eda1b2b598bd6c994acb1b4b35785
Inkeri <inkeri.ahlstrom@helsinki.fi>
2023-06-17 17:58:56 +0300

    Added functions to edit and delete comment, don't work yet though.

--- src/comments.py
+++ src/comments.py
@@ -1,5 +1,5 @@
 from .db import db
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, abort
 from sqlalchemy import text
 
 comments = Blueprint("comments", __name__)
@@ -20,5 +20,39 @@ def send_comment(post_id):
     db.session.execute(sql, {"content":content, "user_id":user_id, "post_id":post_id})
     db.session.commit()
     return redirect(url_for("routes.comments_list", post_id=post_id))
+
+@comments.route("/edit_comment/<int:comment_id>", methods=["GET", "POST"])
+def edit_comment(comment_id):
+    if request.method == "GET":
+        sql = text("SELECT C.id, C.content, C.user_id, C.post_id FROM comments C, users U WHERE U.id=C.user_id AND C.id=:comment_id")
+        edit_comment = db.session.execute(sql, {"comment_id": comment_id})
+        edit_comment2 = edit_comment.fetchone()
+
+        if edit_comment2 is None:
+            return render_template("error.html", message="Comment not found.")
+
+        return render_template("edit_your_comment.html", edit_comment2=edit_comment2)
+
+    elif request.method == "POST":
+        content = request.form.get("content")
+        post_id = request.form.get("post_id")
+
+        sql = text("UPDATE comments SET content=:content WHERE id=:comment_id")
+        db.session.execute(sql, {"content": content, "comment_id": comment_id})
+        db.session.commit()
+
+        return redirect(url_for("routes.comments_list", post_id=post_id))
+
+
+@comments.route("/delete_comment/<int:comment_id>", methods=["GET", "POST"])
+def delete_comment(comment_id):
+    sql = text("DELETE FROM comments WHERE id=:comment_id")
+    result = db.session.execute(sql, {"comment_id": comment_id})
+    db.session.commit()
+
+    if result.rowcount == 0:
+        abort(404)
+
+    return redirect(url_for("routes.comments_list"))
 
  
